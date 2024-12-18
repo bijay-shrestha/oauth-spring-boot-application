f05f17b49ee04df06637a1c0f8538f0c0edd828a
Bijay Shrestha <7867129+bijay-shrestha@users.noreply.github.com>
2024-12-17 18:58:29 -0800

    OAuth application skeleton

---
+++ src/test/java/com/boa/oauthspringbootapplication/OauthSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boa.oauthspringbootapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
